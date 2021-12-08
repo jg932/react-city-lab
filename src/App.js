7e833120e5e07d529c9b32e94eb5db08710556d7
jg932 <jared.h.goldstein@gmail.com>
2021-12-08 14:49:45 -0500

    hurry up

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-// IMPORT useState
 import React, { useState } from "react";
 import "./styles.css";
-import imagesArr from "./imageData.js";
-// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from "./imageData";
+import BigImage from "./BigImage";
+import SmallImage from "./SmallImage"
 
 export default function App() {
+
   const [bigImage, setBigImage] = useState(imagesArr[0].img);
   const handleClick = (imgUrl) => {
     setBigImage(imgUrl);
   };
-  
+
   const images = imagesArr.map((image, index) => {
     return (
-      <img
-      src={image.img}
-      alt={image.city}
-      className="thumbnail"
-      key={index}
-      onClick={() => handleClick(image.img)}
+      <SmallImage
+        style={{ border: bigImage === image.img ? "5px solid green" : "" }}
+        src={image.img}
+        alt={image.city}
+        key={index}
+        className="thumb"
+        handleClick={handleClick}
       />
-    )
-  })
-  // USE useState TO CREATE  [bigImage, setBigImage]
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+    );
+  });
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
-
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key
-  // ALSO ASSIGN AN onClick EVENT THAT CALL THE HANDLE EVENT AND PASSES IT THE IMG URL
+  console.log("images array", images);
 
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">{images}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        <img src={bigImage} id="bigimage" alt='bigImage'/>
+        <div id="thumbnails">
+          {images}
+        </div>
+        <BigImage image={bigImage}/>
       </div>
     </div>
   );
 }
-
---
+++ src/SmallImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const SmallImage = ({city, src, handleClick}) => {
+    return (
+        <img
+        src={src}
+        alt={city}
+        className="thumb"
+        onClick={() => handleClick(src)}
+        />
+    )
+}
+
+export default SmallImage 
